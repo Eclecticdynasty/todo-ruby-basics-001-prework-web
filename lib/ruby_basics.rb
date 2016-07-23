ef724ccda59b9ae60e4824bc82ef5579dcc8105f
Eclecticdynasty <bklyngal1994@gmail.com>
2016-07-23 11:51:30 -0400

    lib/ruby_basics.rb

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
- 
+ 24/4
 end
 
 def assign_variable(value)
-  
+  name = "Bob"
 end
 
-def argue
-  
+def argue (phrase ="I'm right and you are wrong")
+  return phrase
+
 end
+argue
 
-def greeting
-
+def greeting (phrase,name)
+puts "Hey there,Bobby!"
 end
 
 def return_a_value
-
+a = "Nice"
+return a
 end
 
 def last_evaluated_value
 
+siobhan ="expert"
+return siobhan
 end
 
-def pizza_party
+def pizza_party(arg1 ="cheese")
+  return arg1
 
 end
